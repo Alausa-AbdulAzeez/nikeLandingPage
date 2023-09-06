10da065650bdd0cc4b82fb83d7e5b73657a36f86
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-09-06 09:19:25 +0100

    made nav bar

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,11 +1,11 @@
-import CustomerReviews from "./CustomerReviews";
-import Footer from "./Footer";
-import Hero from "./Hero";
-import PopularProducts from "./PopularProducts";
-import Services from "./Services";
-import SpecialOffer from "./SpecialOffer";
-import Subscribe from "./Subscribe";
-import SuperQuality from "./SuperQuality";
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
 
 export {
   CustomerReviews,
@@ -16,4 +16,4 @@ export {
   SpecialOffer,
   Subscribe,
   SuperQuality,
-};
+}
